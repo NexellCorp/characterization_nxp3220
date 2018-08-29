f90937f0cc56c6b34d405ed75f13cc59efc36393
Sungwon Jo <doriya@nexell.co.kr>
2018-08-29 14:18:14 +0900

    devfreq: script: add script for devfreq test
    This patch adds script for devfreq test
    
    Change-Id: Iabb58decef9f73010308c030a886df9c423940b4
    Signed-off-by: Sungwon Jo <doriya@nexell.co.kr>

---
+++ devfreq/nxp3220_devfreq.js
@@ -0,0 +1,18 @@
+{
+	"result"   : "/root",
+	"continue" : false,
+
+	"0": {
+		"id"	   : 0,
+		"descript" : "nx_vpu_test",
+		"thread"   : true,
+		"path"	   : "/usr/bin",
+		"command"  : "nx_vpu_test",
+		"args"     : "-s 1920,1088 -i /usr/data/bitstream -g /usr/data/golden -k 4 -l 200",
+		"return"   : [0, 0],
+		"loop"     : 0,
+		"delay"    : 0,
+		"sleep"    : 0,
+		"logofile" : "nx_vpu_test.txt"
+	}
+}
